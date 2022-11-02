5c9eeb178ee58a9a397ba40d4935d28e4b7f51cc
IULIIA MELNYCHUK <gromova.yv@gmail.com>
2022-11-02 16:29:07 +0200

    Вычислить среднее арифметическое 3 целочисленных значений.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+            int y = 12;
+            int z = 3;
+
+            x += y - x++ * z;
+            z = --x - y * 5;
+            y /= x + 5 % z;
+            z = x++ + y * 5;
+            x = y - x++ * z;
+
+
+
+            Console.WriteLine("{0} {1} {2}", x, y, z);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,15 @@
+﻿static void Calculate(double a, double b, double c)
+{
+    Console.WriteLine("Среднее арифметическое: " + ((a + b + c) / 3).ToString());
+    Console.ReadKey();
+}
+
+double a, b, c;
+Console.WriteLine("Hello, enter 1st value");
+a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Now enter 2nd value");
+b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("And enter 3rd value");
+c = Convert.ToInt32(Console.ReadLine());
+
+Calculate(a, b, c);
