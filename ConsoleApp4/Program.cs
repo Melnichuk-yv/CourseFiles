b76fd473e0644aea6d16dd7d1b30c562a624c66f
IULIIA MELNYCHUK <gromova.yv@gmail.com>
2022-11-04 12:41:12 +0200

    Выравнивание кода

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -1,14 +1,18 @@
-﻿{ 
-const double Pi = 3.14;
-double R, H, V, S;
-Console.WriteLine("Введите радиус цилиндра");
-R = Convert.ToDouble(Console.ReadLine());
-Console.WriteLine("Введите высоту цилиндра");
-H = Convert.ToDouble(Console.ReadLine());
-V = Pi * Math.Pow(R, 2) * H;
-S = (Pi * Math.Pow(R, 2) * H) * 2;
-Console.WriteLine("Определяем объем цилиндра" + V.ToString());
-Console.WriteLine("Определяем площадь поверхности цилиндра" + S.ToString());
-Console.ReadKey();
+﻿{
+    const double Pi = 3.14;
+    double R, H, V, S;
+
+    Console.WriteLine("Введите радиус цилиндра");
+    R = Convert.ToDouble(Console.ReadLine());
+
+    Console.WriteLine("Введите высоту цилиндра");
+    H = Convert.ToDouble(Console.ReadLine());
+
+    V = Pi * Math.Pow(R, 2) * H;
+    S = (Pi * Math.Pow(R, 2) * H) * 2;
+
+    Console.WriteLine("Определяем объем цилиндра" + V.ToString());
+    Console.WriteLine("Определяем площадь поверхности цилиндра" + S.ToString());
+    Console.ReadKey();
 
 }
